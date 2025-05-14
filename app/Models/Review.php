c3e516e2284b45ee745077ba24f522f5b008d02d
thangmai24 <thangmai0107@gmail.com>
2025-05-14 18:32:05 +0700

    create review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
-{
+{//demo
     use HasFactory;
 
     protected $fillable = ['user_id', 'product_id', 'rating', 'comment', 'image'];
