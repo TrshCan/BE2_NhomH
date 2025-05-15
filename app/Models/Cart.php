13a79b5b7ffa0cf1f4f751994447a6b9630e3eae
TrashCan <phuthuytinhyeu1234678@gmail.com>
2025-05-15 10:37:13 +0700

    re-merged changed branches and fixed bugs

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -27,7 +27,7 @@ class Cart extends Model
 
     public function products()
     {
-        return $this->belongsToMany(Product::class, 'cart_product', 'cart_id', 'products_id')
+        return $this->belongsToMany(Product::class, 'cart_product', 'cart_id', 'product_id')
                     ->withPivot('quantity')
                     ->withTimestamps();
     }
